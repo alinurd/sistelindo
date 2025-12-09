29afbd4b7e2072b4e2fc34283f2081e93631e3b5
alinurd <ali.mnrdn14005@gamail.com>
2025-12-09 15:41:42 +0700

    revome view

--- resources/views_/frontend/errors/404.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="card p-3 text-center root">
-    <div class="card-body"> 
-        <img src="{{ asset('assets/img/villa/404.png') }}" />
-         <span class="title">Sorry, the page you requested <br> were not found</span>
-    </div>
-</div>
--- resources/views_/frontend/errors/index.blade.php
+++
@@ -1,4 +0,0 @@
-@include('frontend.components.header')
-@extends('frontend.layout.app') 
-@include('frontend.errors.505', ['page'=>"Booking"])
-
