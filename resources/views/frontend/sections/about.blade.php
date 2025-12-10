da991e71562728460fd22292e551caaff3eb145d
alinurd <ali.mnrdn14005@gamail.com>
2025-12-10 11:00:07 +0700

    fix about end color text-title

--- resources/views/frontend/sections/about.blade.php
+++ resources/views/frontend/sections/about.blade.php
@@ -1,22 +1,15 @@
-<section class="about-section text-center py-5   animate-fade-in">
-
+<section class="about-section text-center py-5 animate-fade-in">
     <div class="container">
-
-                <span class="text-title mb-3 animate-fade-in">About <span class="text-highlight"><strong>Us</strong></span></span>
-
-
-
-        <p class="text-muted mx-auto" style="width: 10px;">
-
-            {!!$company->about !!}
-
-        </p>
-
-
-
-        <a href="#" class="btn btn-primary px-4">Detail</a>
-
+        <span class="text-title mb-2 animate-fade-in">About <span class="text-highlight"><strong>Us</strong></span></span>
+        
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-5">
+                <p class="text-muted mx-auto">
+                    {!!$company->about !!}
+                </p>
+            </div>
+        </div>
+        <a href="#" class="btn btn-primary mt-1">Detail</a>
+        
     </div>
-
 </section>
-
