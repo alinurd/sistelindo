a69b698fcd1f45d2dcc07c5d29169610bcff8654
alinurd <ali.mnrdn14005@gamail.com>
2026-01-12 02:58:26 +0700

    ok

--- resources/views/frontend/productDetail.blade.php
+++ resources/views/frontend/productDetail.blade.php
@@ -9,7 +9,6 @@
             </a>
                        <img src="{{ asset($product->imagebanner ?? 'assets/img/banner-detail-product.jpeg') }}" class="rounded shadow product-image" />
 
-
              <span class="image-caption">
                 Detail Product
             </span>
@@ -47,7 +46,7 @@
         </div>
     </section>
 
-    <section class="mt-5 mb-5" style="color: #064b90;">
+    <section class="mt-5 mb-2" style="color: #064b90;">
         <div class="container bg-light rounded p-4" style="max-width: 700px">
             <p class=" mb-3 text-title" style="font-size: 14px; color: #064b90; font-weight: 500;">
                         Bentuk
@@ -93,9 +92,9 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> 
 
-    <section class="text-center mt-5 px-4">
+    <section class="text-center  px-4">
         <span class="text-title mb-3 animate-fade-in mt-5 text-center">
             Diagram Anomali <br>
             <span class="text-highlight"><strong>Dual Homing (Kurang Ideal)</strong></span>
@@ -112,9 +111,28 @@
             </div>
         </div>
     </section>
+
+
+    <section class="text-center mt-5 px-4">
+        <span class="text-title mb-3 animate-fade-in mt-5 text-center">
+            Company 
+            <span class="text-highlight"><strong>Study Case</strong></span>
+        </span>
+        <div class="container animate-fade-in stagger-delay-1">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <!-- Container untuk gambar timeline -->
+                    <div class="timeline-image-container">
+                        <img src="{{ asset('') }}" class="img-fluid timeline-image "
+                            alt="Study Case Img" loading="lazy">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
     <section class="container text-center">
-
-
      <div class="row justify-content-center g-4">
             <!-- LEFT BOX -->
             <div class="col-md-5 col-lg-4">
