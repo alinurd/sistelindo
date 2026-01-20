bf2faa9461c440d0effd983ef8cdb5ccda185072
alinurd <ali.mnrdn14005@gamail.com>
2026-01-20 19:29:04 +0700

    update navbar

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -1,142 +1,82 @@
 @php
-
     $set = AppSetting::first();
-
 @endphp
-
-{{-- <div class="topbar py-1">
-
-        <div class="container d-flex flex-wrap justify-content-between align-items-center small">
-
-            <div class="d-flex align-items-center gap-3">
-
-            </div>
-
-            <div class="social-icons mt-1">
-
-                <a  class="me-2">
-
-                    <img src="{{ asset('assets/img/icons/id.png') }}" width="40" alt="Bahasa" class="me-1"> In
-
-                </a>
-
-                <a  class="me-2">
-
-                    <img src="{{ asset('assets/img/icons/en.png') }}" width="40" alt="English" class="me-1"> En
-
-                </a> 
-
-            </div>
-
-        </div>
-
-    </div> --}}
-
 <nav class="navbar navbar-expand-lg bg-white">
-
     <div class="container">
-
-
-
         <a class="navbar-brand fw-bold text-primary" href="{{ route('guest.home') }}">
-
-             <img src="{{ $set?->logo ? asset($setting->logo) : asset('/storage/photos/1/sistelindo-favicon.png') }}"  alt="{{$set?->title? : 'Sistelindo'}}"
-
-                        height="60">
-
+            <img src="{{ $set?->logo ? asset($setting->logo) : asset('/storage/photos/1/sistelindo-favicon.png') }}"  
+                 alt="{{ $set?->title ?: 'Sistelindo' }}" height="60">
         </a>
 
-
-
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav">
-
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
             <span class="navbar-toggler-icon"></span>
-
         </button>
 
-
-
         <div id="nav" class="collapse navbar-collapse">
-
-    <ul class="navbar-nav ms-auto" style="font-size:12px ">
-
-
-
-        <li class="nav-item" >
-
-            <a class="nav-link" href="{{ route('guest.home') }}">Home</a>
-
-        </li>
-
-
-
-        <li class="nav-item">
-
-            <a class="nav-link" href="{{ route('guest.about') }}">About Us</a>
-
-        </li>
-
-        <li class="nav-item">
-
-            <a class="nav-link" href="{{ route('guest.product') }}">Products & Services</a>
-
-        </li>
-
-
-
-        <!-- Dropdown Submenu -->
-
-        {{-- <li class="nav-item dropdown">
-
-            <a class="nav-link dropdown-toggle" href="#" id="productsDropdown" role="button"
-
-                data-bs-toggle="dropdown" aria-expanded="false">
-
-                Products & Services
-
-            </a>
-
-
-
-            <ul class="dropdown-menu" aria-labelledby="productsDropdown">
-
-                 <li><a class="dropdown-item" href="{{ route('guest.subproduct', 1) }}">Service 1</a></li>
-
-                 <li><a class="dropdown-item" href="{{ route('guest.subproduct', 2) }}">Service 1</a></li>
-
-                 <li><a class="dropdown-item" href="{{ route('guest.subproduct', 3) }}">Service 1</a></li>
-
-
-
-                <li><hr class="dropdown-divider"></li>
-
-
-
-                <li><a class="dropdown-item" href="{{ route('guest.product') }}">Other</a></li>
-
+            <ul class="navbar-nav ms-auto" style="font-size:12px;">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('guest.home') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('guest.about') }}">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('guest.product') }}">Products & Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('guest.contact') }}">Contact Us</a>
+                </li>
             </ul>
-
-        </li> --}}
-
-
-
-        <li class="nav-item">
-
-            <a class="nav-link" href="{{ route('guest.contact') }}">Contact Us</a>
-
-        </li>
-
-
-
-    </ul>
-
-</div>
-
-
-
-
-
+        </div>
     </div>
-
 </nav>
 
+<style>
+    /* CSS untuk memperbaiki navbar di mobile */
+    @media (max-width: 991.98px) {
+        .navbar-collapse {
+            position: absolute;
+            top: 100%;
+            left: 0;
+            right: 0;
+            background-color: #fff;
+            z-index: 1000;
+            box-shadow: 0 5px 10px rgba(0,0,0,0.1);
+            border-top: 1px solid #eee;
+        }
+        
+        .navbar-nav {
+            padding: 1rem 0;
+        }
+        
+        .nav-item {
+            border-bottom: 1px solid #f5f5f5;
+        }
+        
+        .nav-item:last-child {
+            border-bottom: none;
+        }
+        
+        .nav-link {
+            padding: 0.75rem 1.5rem;
+            font-size: 14px !important;
+        }
+        
+        /* Pastikan navbar brand tidak terlalu besar di mobile */
+        .navbar-brand img {
+            height: 50px !important;
+            max-height: 50px;
+        }
+    }
+    
+    @media (max-width: 575.98px) {
+        .navbar-brand img {
+            height: 40px !important;
+        }
+        
+        .container {
+            padding-left: 15px;
+            padding-right: 15px;
+        }
+    }
+</style>
